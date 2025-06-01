bc13f1108160cdeede4159f0922e2cbae43810e9
mc095 <ganeshvathumilli@gmail.com>
2025-06-01 22:05:35 +0530

    fixes

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHv6Ry8eYJW-A-owwrfa8MvOte9Q3SQsA",
-  authDomain: "dsa-tracker-9cc8d.firebaseapp.com",
-  projectId: "dsa-tracker-9cc8d",
-  storageBucket: "dsa-tracker-9cc8d.appspot.com",
-  messagingSenderId: "998480286838",
-  appId: "1:998480286838:web:22d689acdfbdbd9f167b57"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
